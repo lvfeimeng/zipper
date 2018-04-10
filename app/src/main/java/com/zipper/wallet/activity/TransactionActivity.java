455b427590a8be392e3bf30083fbeeb4d10b0cd4
ShitUP8 <13718122286@163.com>
2018-04-10 19:23:38 +0800

    //交易记录界面

---
+++ app/src/main/java/com/zipper/wallet/activity/TransactionActivity.java
@@ -0,0 +1,49 @@
+package com.zipper.wallet.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+
+import com.zipper.wallet.R;
+import com.zipper.wallet.adapter.TransactionHistoryAdapter;
+import com.zipper.wallet.bean.TransactionBean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TransactionActivity extends AppCompatActivity {
+    private RecyclerView mRecyclehistory;
+    private Toolbar mToolbar;
+    private List<TransactionBean> mList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transaction);
+        initView();
+    }
+
+    private void initView() {
+
+        mRecyclehistory = findViewById(R.id.recycler_history);
+        mToolbar = findViewById(R.id.toolbar_transaction);
+        setSupportActionBar(mToolbar);
+        mRecyclehistory.setLayoutManager(new LinearLayoutManager(this));
+        mList = new ArrayList<>();
+        testData();
+        TransactionHistoryAdapter adapter = new TransactionHistoryAdapter(mList,this);
+        mRecyclehistory.setAdapter(adapter);
+
+    }
+
+    private void testData() {
+
+        mList.add(new TransactionBean("ETH","03月16日","确认次数：333","+ 1999.999 ETH"));
+        mList.add(new TransactionBean("SMT","02月06日","确认次数：222","+ 2999.999 SMT"));
+        mList.add(new TransactionBean("BTC","01月26日","确认次数：111","+ 3999.999 BTC"));
+        mList.add(new TransactionBean("SMT","12月06日","确认次数：000","+ 4999.999 SMT"));
+        mList.add(new TransactionBean("ZIP","11月16日","确认次数：111","+ 5999.999 ZIP"));
+
+    }
+}
