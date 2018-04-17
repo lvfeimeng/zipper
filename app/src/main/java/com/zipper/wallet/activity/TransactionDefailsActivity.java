5dad8856138eef859edc26b2990497490399fc3f
ShitUP8 <13718122286@163.com>
2018-04-17 14:26:51 +0800

    //界面实时更新，待接口

--- app/src/main/java/com/zipper/wallet/activity/TransactionDefailsActivity.java
+++ app/src/main/java/com/zipper/wallet/activity/TransactionDefailsActivity.java
@@ -3,6 +3,8 @@ package com.zipper.wallet.activity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -20,6 +22,18 @@ public class TransactionDefailsActivity extends BaseActivity implements View.OnC
     private TextView mTextState;
     private LinearLayout mLinerGradient;
     private ImageView mImgState;
+
+    private int IN = 1;
+    private int OUT = 2;
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            updateState(msg.what);
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,37 +53,79 @@ public class TransactionDefailsActivity extends BaseActivity implements View.OnC
         mTextState = findViewById(R.id.text_state);
         mLinerGradient = findViewById(R.id.liner_gradient);
         mImgState = findViewById(R.id.img_state);
-//        updateState();
         mBack.setOnClickListener(this);
         mTextUpdate.setOnClickListener(this);
 
         mDetailsCurrency.setText(mCurrency);
 
+        if (mCurrency.contains("+")) {
+            mTextState.setText("等待转入");
+            handlerThread(IN);
+        } else if (mCurrency.contains("-")) {
+            handlerThread(OUT);
+        }
+
 
     }
 
+    private void handlerThread(int num) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Message msg = new Message();
+                switch (num) {
+
+                    case 1:
+                        msg.what = 1;
+                        mHandler.sendMessage(msg);
+                        break;
+
+                    case 2:
+                        msg.what = 2;
+                        mHandler.sendMessage(msg);
+                        break;
+
+                }
+            }
+        }, 1000);
+    }
+
     @SuppressLint("ResourceAsColor")
-    private void updateState() {
-        //收入成功
-        mTextState.setText("已收入");
-        mTextState.setTextColor(R.color.color_button_blue);
-        mLinerGradient.setBackgroundResource(R.drawable.wallet_ok_bg);
-        mImgState.setBackgroundResource(R.mipmap.transaction_ok);
-        mTextUpdate.setGravity(View.GONE);
+    private void updateState(int details) {
+        switch (details) {
 
-        //转出成功
-        mTextState.setText("已转出");
-        mTextState.setTextColor(R.color.color_button_blue);
-        mLinerGradient.setBackgroundResource(R.drawable.wallet_ok_bg);
-        mImgState.setBackgroundResource(R.mipmap.transaction_ok);
-        mTextUpdate.setGravity(View.GONE);
+            case 1:
+                //收入成功
+                mTextState.setText("已收入");
+                mTextState.setTextColor(R.color.color_button_blue);
+                mLinerGradient.setBackgroundResource(R.drawable.wallet_ok_bg);
+                mImgState.setBackgroundResource(R.mipmap.transaction_ok);
+                mTextUpdate.setGravity(View.GONE);
+                break;
 
-        //等待转出
-        mTextState.setText("等待转出");
-        mTextState.setTextColor(R.color.text_minor);
-        mLinerGradient.setBackgroundResource(R.drawable.wallet_inprocess_bg);
-        mImgState.setBackgroundResource(R.mipmap.inprocess);
-        mTextUpdate.setGravity(View.VISIBLE);
+            case 2:
+                //转出成功
+                mTextState.setText("已转出");
+                mTextState.setTextColor(R.color.color_button_blue);
+                mLinerGradient.setBackgroundResource(R.drawable.wallet_ok_bg);
+                mImgState.setBackgroundResource(R.mipmap.transaction_ok);
+                mTextUpdate.setGravity(View.GONE);
+                break;
+
+            case 3:
+                //等待转出
+                mTextState.setText("等待转出");
+                mTextState.setTextColor(R.color.text_minor);
+                mLinerGradient.setBackgroundResource(R.drawable.wallet_inprocess_bg);
+                mImgState.setBackgroundResource(R.mipmap.inprocess);
+                mTextUpdate.setGravity(View.VISIBLE);
+                break;
+
+            default:
+                break;
+
+        }
+
     }
 
     @Override
